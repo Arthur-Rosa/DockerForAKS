2f217fe7c7a8f2b4c4c2deb0c8cc13f76eba0305
Arthur-Rosa <arthur.corporativo@outlook.com>
2023-01-04 09:37:02 -0300

    initial :sparkles:

---
+++ aquarelacs-front/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      Olá pessoal da Aquarela! Aqui é o Arthur!!!!
+    </div>
+  );
+}
+
+export default App;
